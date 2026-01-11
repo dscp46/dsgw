ed19240a3a2eaeb365e5e8e253df4c5174f4876b
dscp46 <celestine+dev@gramaize.eu>
2026-01-11 21:37:25 +0100

    Remove debug prompt

--- src/dv_frame.c
+++ src/dv_frame.c
@@ -12,7 +12,6 @@ int dv_radio_invalid_csum ( dv_radio_hdr_t *hdr)
 	const size_t hdr_sz = sizeof( dv_radio_hdr_t);
 	uint16_t read_csum = le16toh( hdr->p_fcs);
 	uint16_t computed_csum = crc_ccitt( (uint8_t*) hdr, hdr_sz-2);
-	printf( "Expected: %04x, Actual: %04x\n", read_csum, computed_csum);
 	return ( read_csum != computed_csum );
 }
 
