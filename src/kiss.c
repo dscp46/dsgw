8cb9e044aeb8021535d61a4a463739b6af5e6c05
dscp46 <celestine+dev@gramaize.eu>
2026-01-17 22:59:52 +0100

    Prepare KISS reader thread

--- src/kiss.c
+++ src/kiss.c
@@ -1,6 +1,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
@@ -8,7 +9,23 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#include "dextra.h"
 #include "kiss.h"
+
+/***** Internal functions  *****/
+typedef struct kiss_queue_entry {
+	struct kiss_queue_entry *prev;
+	struct kiss_queue_entry *next;
+
+	void         *packet;
+	size_t        length;
+	unsigned char ackmode_id[2];
+	uint8_t       type;
+} kiss_queue_entry_t;
+
+kiss_queue_entry_t *kiss_queue_pop( kiss_queue_entry_t *self);
+void kiss_queue_flush( kiss_queue_entry_t *self);
+/*******************************/
 
 // Set up a new KISS connection
 //   When port is set to 0, an AF_UNIX socket is used.
@@ -96,4 +113,89 @@ int kiss_new_connection( const char *addr, uint16_t port)
 	}
 
 	return sock_fd;
+}
+
+#define KISS_RXBUF_SZ	1360
+
+void* kiss_receiver( void* argv)
+{
+	dextra_peer_t *peer = (dextra_peer_t *)argv;
+	if( peer == NULL )
+	{
+		fprintf( stderr, "%s: Null pointer received.\n", __FUNCTION__);
+		return NULL;
+	}
+	
+	ssize_t ret_code;
+	kiss_queue_entry_t *queue = NULL;
+	char buffer[KISS_RXBUF_SZ];
+
+	while( 1)
+	{
+		ret_code = read(
+			peer->child_fd,
+			buffer,
+			KISS_RXBUF_SZ
+		);
+
+		if( ret_code < 0 )
+		{
+			if( ret_code == EAGAIN || ret_code == EWOULDBLOCK )
+			{
+				usleep(1);
+				continue;
+			}
+
+			break;
+		}
+
+		if( ret_code < 3 ) continue; // Ignore runts
+
+	}
+
+	kiss_queue_flush( queue);
+
+	return NULL;
+}
+kiss_queue_entry_t *kiss_queue_add( kiss_queue_entry_t *self, const void* buffer, size_t len)
+{
+	kiss_queue_entry_t *instance = (kiss_queue_entry_t*) malloc( sizeof( kiss_queue_entry_t));
+	instance->packet = (unsigned char*) malloc( len);
+	
+	// TODO: Evaluate and decode frame
+	
+	memcpy( instance->packet, buffer, len);
+	instance->length = len;
+
+	instance->next = self;
+	instance->prev = NULL;
+	if( self != NULL )
+		self->prev = instance;
+
+	return instance;
+}
+
+kiss_queue_entry_t *kiss_queue_pop( kiss_queue_entry_t *self)
+{
+	kiss_queue_entry_t *entry = self;
+	if( self == NULL ) return NULL;
+
+	while( entry->next != NULL )
+		entry = entry->next;
+	
+	entry->prev = NULL;
+	return entry;
+}
+
+void kiss_queue_flush( kiss_queue_entry_t *self)
+{
+	kiss_queue_entry_t *entry = NULL;
+
+	while((entry = kiss_queue_pop( self)) != NULL)
+	{
+		if( entry->packet != NULL )
+			free( entry->packet);
+
+		free( entry);
+	}
 }
--- src/kiss.h
+++ src/kiss.h
@@ -5,6 +5,12 @@
 
 #define KISS_DEFAULT_PORT	8100
 
+#define KISS_CMD_MASK		0x0F
+
+#define KISS_CMD_DATA		0x00
+#define KISS_CMD_ACKMODE	0x0C
+
 int kiss_new_connection( const char *addr, uint16_t port);
+void* kiss_receiver( void* argv);
 
 #endif	/* __KISS_H */
